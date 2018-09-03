a26907a5890da75c6f0770e81edfa85fbbabf906
shen9209 <shen9209@gmail.com>
2018-09-03 08:51:46 +0800

    fixed

--- BarcodeLibTest/Program.cs
+++ BarcodeLibTest/Program.cs
@@ -13,7 +13,7 @@ namespace BarcodeLibTest
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new TestApp())
+            Application.Run(new TestApp());
         }
     }
 }
